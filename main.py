04eae507c94672ef2f643d1c37ba5230eb00349b
RylanOC <rylanoc@gmail.com>
2018-03-22 21:20:06 -0400

    Add documentation and refactor variable names

--- main.py
+++ main.py
@@ -7,60 +7,70 @@ import operator
 import cv2
 import numpy as np
 
-# detect all video capture devices currently connected
-# returns list of camera objects
-def detect_cameras():
-	num_cams = 0
-	caps = []
-	while True:
-		print("Checking device {}...".format(num_cams))
-		cap = cv2.VideoCapture(num_cams)
-		ret, _ = cap.read()
-		print(ret)
-		if ret == True:
-			print("Success!")
-			caps.append(cap)
-			num_cams += 1
-		else:
-			print("Detected {} devices.".format(num_cams))
-			return caps
 
-# spawns two windows per connected camera to show raw/processed footage
-# accepts the number of connected cameras as argument
-def setup_windows(num_cams):
-	for cam_num in range(0, num_cams):
-		cv2.namedWindow("cam{} raw".format(cam_num))
-		cv2.namedWindow("cam{} avg".format(cam_num))
+'''
+@requires None
+@modifies None
+@returns List of VideoCapture objects
+detect all video capture devices currently connected
+returns list of camera objects
+'''
+def detect_cameras():
+	num_cameras = 0
+	cameras = []
+	while True:
+		print("Checking device {}...".format(num_cameras))
+		capture = cv2.VideoCapture(num_cameras)
+		success, frame = capture.read()
+		if success == True:
+			print("Success!")
+			cameras.append(capture)
+			num_cameras += 1
+		else:
+			print("Detected {} devices.".format(num_cameras))
+			return cameras
+
+'''
+@requires number of connected cameras
+@modifies None
+@returns None
+spawns two windows per connected camera to show raw/processed footage
+accepts the number of connected cameras as argument
+'''
+def setup_windows(num_cameras):
+	for camera_num in range(0, num_cameras):
+		cv2.namedWindow("camera {} raw".format(camera_num))
+		cv2.namedWindow("camera {} avg".format(camera_num))
 
 # main loop to read camera data and detect occupancy
 # accepts list of VideoCapture objects as argument
-def detect_occupancy(caps):
-	num_cams = len(caps)
+def detect_occupancy(cameras):
+	num_cameras = len(cameras)
 	background = (0,0,0) # base value to compare new readings to
-	thresh = 5 # threshold percent for occupancy
+	threshold = 5 # threshold percent for occupancy
 
 	while True:
-		for cam_num in range(0, num_cams):
-			ret, img = caps[cam_num].read()
-			if not ret:
+		for camera_num in range(0, num_cameras):
+			success, frame = cameras[camera_num].read()
+			if not success:
 				return
 
-			cv2.imshow("cam{} raw".format(cam_num), img)
+			cv2.imshow("cam{} raw".format(camera_num), frame)
 
 			# calculate and display average color across image
-			avg = img
-			average_color = [img[:, :, i].mean() for i in range(img.shape[-1])]	
-			avg[:]  = average_color
-			cv2.imshow("cam{} avg".format(cam_num), img)
+			averaged_frame = frame
+			average_color = [frame[:, :, i].mean() for i in range(frame.shape[-1])]	
+			averaged_frame[:]  = average_color
+			cv2.imshow("cam{} avg".format(camera_num), frame)
 
 			change = np.subtract(background, average_color)
 			percent_change = abs(sum(change)/len(change)/255*100)
 			
 			state = "Unoccupied"
-			if (percent_change > thresh):
+			if (percent_change > threshold):
 				state = "Occupied"
 
-			print ("CAM{}        Percent Change: {:.2f}%    State: {}".format(cam_num, percent_change, state))
+			print ("CAM{}        Percent Change: {:.2f}%    State: {}".format(camera_num, percent_change, state))
 		
 		# check user input
 		# b: update background
@@ -73,14 +83,14 @@ def detect_occupancy(caps):
 
 # frees VideoCapture objects and closes windows to ensure exit goes smoothly
 # accepts a list of VideoCapture objects as argument
-def free(caps):
+def free(cameras):
 	# free resources
-	for cap in caps:
+	for cap in cameras:
 		cap.release()
 	cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-	caps = detect_cameras()
-	setup_windows(len(caps))
-	detect_occupancy(caps)
-	free(caps)
+	cameras = detect_cameras()
+	setup_windows(len(cameras))
+	detect_occupancy(cameras)
+	free(cameras)
